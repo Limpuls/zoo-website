c3f272ff0f4c0d3ffa1c8205841a5b9034dd1fb7
Limpuls <Limpuls17@gmail.com>
2016-09-28 13:57:59 +0200

    homepage
    home is almost done, also created all the additional pages with assets.

---
+++ script.js
@@ -0,0 +1,36 @@
+//var nav = document.getElementById("normal_menu");
+//var myElement = document.getElementById("la");
+//myElement.addEventListener("click", function () {
+
+  //document.getElementsByClassName('whatever').style.display = "block";
+
+//});
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+//var myElement = document.getElementsByTag("a");
+//function someFunction() {
+  //if (a.href == window.location.href) {
+  //  a.classList.add ("otherclass");
+//  }
+//}
+//var element = document.getElementsByTag ("header");
+//function someFunction() {
+//  if(a.href == window.location.href) {
+  //  this.classList.add("otherclass");
+//  }  else {
+  //  alarm("kurwa");
+  //}
+//}
+//element.addEventListener("click" function () {
+
+  //document.getElementsByTag("a").className += " otherclass";
+
+//});
